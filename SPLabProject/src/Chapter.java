8e80c9cc046d5945ccbee8623cf74a2f4e49643e
IoanaArghisan <59994654+IoanaArghisan@users.noreply.github.com>
2021-10-11 14:15:21 +0300

    Lab3 implementation

---
+++ SPLabProject/src/Chapter.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Chapter {
+	private String name;
+	
+	private ArrayList<SubChapter> subChapters = new ArrayList<>();
+	
+	public Chapter(String name) {
+		this.name = name;
+	}
+	
+
+	public int createSubChapter(String name) {
+		SubChapter sub = new SubChapter(name);
+		subChapters.add(sub);
+		return subChapters.indexOf(sub);
+	}
+	
+	public SubChapter getSubChapter(int index) {
+		return subChapters.get(index);
+	}
+	
+	public void print() {
+		System.out.println("Chapter name is "+this.name);
+	}
+
+}
