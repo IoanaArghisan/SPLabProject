222a00ed073803f41952ed3c7bdcbe7f4f049d45
IoanaArghisan <59994654+IoanaArghisan@users.noreply.github.com>
2021-10-25 16:58:21 +0300

    Add files via upload

---
+++ SPLabProject/src/Image.java
@@ -0,0 +1,13 @@
+
+public class Image implements Element {
+	private String imageName;
+	
+	public Image(String imageName) {
+		this.imageName = imageName;
+	}
+	
+	public void print() {
+		System.out.println("Image with name: "+this.imageName);
+	}
+
+}
