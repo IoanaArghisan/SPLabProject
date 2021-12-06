faa6eb3e9e82c6059c1ea28f004adfb1b141cf2f
IoanaArghisan <59994654+IoanaArghisan@users.noreply.github.com>
2021-12-06 09:54:23 +0200

    singleton_implementation

---
+++ SPLabProject/src/Paragraph.java
@@ -0,0 +1,22 @@
+
+public class Paragraph implements Element {
+	private String name;
+	private AlignStrategy alignStrategy;
+	
+	public Paragraph(String name) {
+		this.name = name;
+	}
+	
+	public void print() {
+		if(alignStrategy == null) {
+			System.out.println(this.name);
+		}else {
+			alignStrategy.render(this.name);
+		}
+	}
+	
+	public void setAlignStrategy(AlignStrategy alignStrategy) {
+		this.alignStrategy = alignStrategy;
+	}
+
+}
